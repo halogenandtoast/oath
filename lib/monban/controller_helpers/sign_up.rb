bdc5238e92bb4b70145a7ec41d07826d6ca22d43
halogenandtoast <halogenandtoast@gmail.com>
2013-02-20 21:45:53 -0500

    Fix setting of password.

--- lib/monban/controller_helpers/sign_up.rb
+++ lib/monban/controller_helpers/sign_up.rb
@@ -1,7 +1,7 @@
 module Monban
   class SignUp
     def initialize user_params
-      unencrypted_token = user_params.slice(token_field)
+      unencrypted_token = user_params[token_field]
       token_digest = Monban.encrypt_token(unencrypted_token)
       @user_params = user_params.except(token_field).merge(token_store_field.to_sym => token_digest)
     end
--- spec/monban/controller_helpers/sign_up_spec.rb
+++ spec/monban/controller_helpers/sign_up_spec.rb
@@ -5,11 +5,11 @@ describe Monban::SignUp, '#perform' do
   it 'creates a user with the right parameters' do
     create = double
     stub_const('User', create)
-    user_params = { email: 'email@example.com', password: 'password' }
+    user_params = { 'email' => 'email@example.com', 'password' => 'password' }
 
     create.should_receive(:create) do |args|
       args[:email].should eq(user_params[:email])
-      args.should have_key(:password_digest)
+      Monban.compare_token(args[:password_digest], 'password').should be_true
     end
 
     Monban::SignUp.new(user_params).perform
