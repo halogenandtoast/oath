f9ad5a0526d128b0381560a5c1e2fc8919610b3b
Talal Alnafie <alnafie@gmail.com>
2013-07-02 17:07:44 +0300

    fixes error on trying to respond with http 401

--- lib/monban/railtie.rb
+++ lib/monban/railtie.rb
@@ -4,6 +4,7 @@ module Monban
   class Railtie < Rails::Railtie
     config.app_middleware.use Warden::Manager do |config|
       Monban.initialize(config)
+      config.failure_app = lambda{|e|[401, {"Content-Type" => "text/plain"}, ["Authorization Failed"]] }
     end
   end
 end
