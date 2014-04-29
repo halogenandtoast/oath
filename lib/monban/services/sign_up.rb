52861649312cbce73332c707ce8ba8b232d2f4b0
Avi Gilligan <euraldius@gmail.com>
2014-04-29 15:52:04 -0400

    Prevent hasing of empty string passwords

--- lib/monban/services/sign_up.rb
+++ lib/monban/services/sign_up.rb
@@ -1,9 +1,10 @@
 module Monban
   class SignUp
     def initialize user_params
-      unencrypted_token = user_params[token_field]
-      token_digest = Monban.encrypt_token(unencrypted_token)
-      @user_params = user_params.except(token_field).merge(token_store_field.to_sym => token_digest)
+      encrypted_token = token_digest(user_params)
+      @user_params = user_params.
+        except(token_field).
+        merge(token_store_field.to_sym => encrypted_token)
     end
 
     def perform
@@ -11,6 +12,13 @@ module Monban
     end
 
     private
+
+    def token_digest(user_params)
+      unencrypted_token = user_params[token_field]
+      unless unencrypted_token.empty?
+        Monban.encrypt_token(unencrypted_token)
+      end
+    end
 
     def token_store_field
       Monban.config.user_token_store_field
