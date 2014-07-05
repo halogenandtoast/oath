f57f4ca896b72eb530d9060341a933e93ef1fad0
halogenandtoast <halogenandtoast@gmail.com>
2014-07-04 20:05:02 -0400

    Fix PasswordStragegy to use config options
    * Now uses Monban.config.user_lookup_field
    * Now uses Monban.config.user_token_field

--- lib/monban/strategies/password_strategy.rb
+++ lib/monban/strategies/password_strategy.rb
@@ -4,13 +4,31 @@ module Monban
   module Strategies
     class PasswordStrategy < ::Warden::Strategies::Base
       def valid?
-        params[:email] || params[:password]
+        lookup_field_value || token_field_value
       end
 
       def authenticate!
-        user = Monban.user_class.find_by(email: params[:email])
-        auth = Authentication.new(user, params[:password])
+        user = Monban.user_class.find_by(lookup_field => lookup_field_value)
+        auth = Authentication.new(user, token_field_value)
         auth.authenticated? ? success!(user) : fail!("Could not log in")
+      end
+
+      private
+
+      def lookup_field_value
+        params[lookup_field]
+      end
+
+      def token_field_value
+        params[token_field]
+      end
+
+      def lookup_field
+        Monban.config.user_lookup_field
+      end
+
+      def token_field
+        Monban.config.user_token_field
       end
     end
   end
